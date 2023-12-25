d98b08c5ceb35ede35aa92e1e76e4ecb1f446762
Aspetuur <ivananishhenkov@gmail.com>
2023-12-25 16:08:58 +0300

    !

---
+++ Learn_1/Learn_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string symbols;
+
+        using (FileStream fstream = File.OpenRead("task1.txt"))
+        {
+            byte[] buffer = new byte[fstream.Length];
+            fstream.Read(buffer, 0, buffer.Length);
+
+            symbols = System.Text.Encoding.UTF8.GetString(buffer);
+        }
+
+        int max = 0, counter = 0;
+
+        for (int i = 0; i < symbols.Length; i++)
+        {
+            if (symbols[i] == 'A' || symbols[i] == 'E')
+            {
+                if (counter > max)
+                {
+                    max = counter;
+                }
+
+                counter = 0;
+            }
+            else
+            {
+                counter++;
+            }
+        }
+
+        Console.WriteLine($"Длина длинейшей подцепочки без гласных в строке - {max}");
+    }
+}
+
